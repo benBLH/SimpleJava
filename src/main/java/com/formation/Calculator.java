f08f2c31b2aa129d39e0213e2b1f24ead4f192f7
abdenour <bellache.abdenour@gmail.com>
2021-05-12 16:32:42 +0200

    second commit

--- src/main/java/com/formation/Calculator.java
+++ src/main/java/com/formation/Calculator.java
@@ -1,8 +1,8 @@
 package com.formation;
 
 public class Calculator {
-	public int add(int a, int b) {
-		return a+b;
+	public int add(int x, int y) {
+		return x+y;
 	}
 	public int multiply(int a, int b) {
 		return a*b;
